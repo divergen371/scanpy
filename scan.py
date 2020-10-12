e6b6ce99268c6234c0ab04d253d685cce38e8a20
divergen371 <divergen@protonmail.com>
2020-10-12 19:13:09 +0900

    update

--- scan.py
+++ scan.py
@@ -1,5 +1,7 @@
 from logging import getLogger, ERROR
 
+from scapy.layers.inet import ICMP, IP, TCP
+
 getLogger("scapy.runtime").setLevel(ERROR)
 from scapy.all import *
 import sys
@@ -13,7 +15,7 @@ try:
     max_port = input("[*] Enter Max Port Number: ")
 
     try:
-        if int(min_port) >= 0 and int(max_port) >= 0 and int(max_port) >= int(min_port):
+        if 0 <= int(min_port) <= int(max_port) and int(max_port) >= 0:
             pass
         else:
             print("\n[!] Invalid Range of Ports")
@@ -27,3 +29,48 @@ except KeyboardInterrupt:
     print("\n[*] User Requested Shutdown...")
     print("[*] Exiting...")
     sys.exit(1)
+
+ports = range(int(min_port), int(max_port) + 1)
+start_clock = datetime.now()
+SYNACK = 0x12
+RSTACK = 0x14
+
+
+def checkhost(ip):
+    conf.verb = 0
+    try:
+        ping = sr1(IP(dst=ip) / ICMP())
+        print("\n[*] Traget is Up, Beginning Scan...")
+    except Exception:
+        print("\n[!] Couldn't Resolve Target")
+        print("[!] Exiting...")
+        sys.exit(1)
+
+
+def scanport(port):
+    srcport = RandShort()
+    conf.verb = 0
+    SYNACKpkt = sr1(IP(dst=target) / TCP(sport=srcport, dport=port, flags="S"))
+    pktflags = SYNACKpkt.getlayer(TCP).flags
+    if pktflags == SYNACK:
+        return True
+    else:
+        return False
+
+    RSTpkt = IP(dst=target) / TCP(sport=srcport, dport=port, flags="R")
+    send(RSTpkt)
+
+
+checkhost(target)
+print("[*] Scanning Started at " + strftime("%H:%M:%S") + "!\n")
+
+for port in ports:
+    status = scanport(port)
+    if status == True:
+        print("Port " + str(port) + ": Open")
+
+
+stop_clock = datetime.now()
+total_time = stop_clock - start_clock
+print("\n[*] Scanning Finished:)")
+print("[*] Total Scan Duration: " + str(total_time))
