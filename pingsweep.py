016fae2fef00f367b44e8722a62acff8449f1481
aisikawaa <alterthemadness@outlook.com>
2020-12-04 00:02:34 +0900

    update

--- pingsweep.py
+++ pingsweep.py
@@ -1,16 +1,14 @@
-from datetime import datetime
-import threading
 import sys
-import re
+import threading
+from datetime import datetime
 from logging import ERROR, getLogger
 from queue import Queue
 
-from scapy.config import conf
-from scapy.layers.inet import IP, ICMP, TCP
-from scapy.sendrecv import sr, sr1
-
 import netaddr
-from scapy.volatile import RandShort
+from scapy.config import conf
+from scapy.layers.inet import ICMP, IP, TCP
+from scapy.layers.l2 import ARP, ARPingResult, Ether
+from scapy.sendrecv import sr1, srp
 
 getLogger("scapy.runtime").setLevel(ERROR)
 try:
@@ -29,19 +27,17 @@ except KeyboardInterrupt:
 
 iface = conf.iface
 time_out = 3
-waking_host = []
 print_lock = threading.Lock()
 
 
-def icmp_ping(ip: int):
+def icmp_ping(ip: int) -> None:
     """
     Generate a packet containing the ICMP header and Confirmation of control messages.
     ip: int
         Figures taken from the worker. Based on this, retrieve the target from "all_host".
     """
-    reply = sr1(
-        IP(dst=str(all_hosts[ip])) / ICMP(), timeout=time_out, iface=iface, verbose=0
-    )
+    conf.verb = 0
+    reply = sr1(IP(dst=str(all_hosts[ip])) / ICMP(), timeout=time_out, iface=iface)
     with print_lock:
         if reply is None:
             print("[*] {} is not running.".format(all_hosts[ip]))
@@ -53,46 +49,48 @@ def icmp_ping(ip: int):
             print("[*] {} is down".format(all_hosts[ip]))
         else:
             print("[*] {} is waking".format(all_hosts[ip]))
-            waking_host.append(all_hosts[ip])
 
 
-def syn_ping(ip):
+def syn_ping(ip: int) -> None:
+    conf.verb = 0
     SYNACK = 0x12
     RSTACK = 0x14
     reply = sr1(
         IP(dst=str(all_hosts[ip])) / TCP(dport=80, flags="S"),
         timeout=time_out,
         iface=iface,
-        verbose=0,
     )
     with print_lock:
         if reply is None:
-            print("[*] {} is not running.".format(all_hosts[ip]))
+            pass
         elif int(reply.getlayer(TCP).flags) in [SYNACK, RSTACK]:
             print("[*] {} is waking".format(all_hosts[ip]))
-            waking_host.append(all_hosts[ip])
-        else:
-            print("[*] {} is state unknown.")
 
 
-def ack_ping(ip):
+def ack_ping(ip: int) -> None:
+    conf.verb = 0
     RST = 0x04
     reply = sr1(
         IP(dst=str(all_hosts[ip])) / TCP(dport=80, flags="A"),
         timeout=time_out,
-        verbose=0,
     )
     with print_lock:
         if reply is None:
-            print("[*] {} is not running.".format(all_hosts[ip]))
+            pass
         elif int(reply.getlayer(TCP).flags) == RST:
             print("[*] {} is waking.".format(all_hosts[ip]))
-            waking_host.append(all_hosts[ip])
-        else:
-            print("[*] {} is state unknown.")
 
 
-def arp_ping
+def arp_ping(ip: int) -> None:
+    conf.verb = 0
+    ans, unans = srp(
+        Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=str(all_hosts[ip])),
+        timeout=time_out,
+    )
+    with print_lock:
+        reply = ARPingResult(ans.res)
+        reply.show()
+
 
 q = Queue()
 
@@ -106,8 +104,9 @@ def threader():
     while True:
         worker = q.get()
         # icmp_ping(worker)
-        ack_ping(worker)
+        # ack_ping(worker)
         # syn_ping(worker)
+        arp_ping(worker)
         q.task_done()
 
 
@@ -126,5 +125,4 @@ if __name__ == "__main__":
     q.join()
 
     total_duration = datetime.now() - start_time
-    print("[*] number of waking host {}".format(len(waking_host)))
     print("[*]Total time duration: " + str(total_duration))
