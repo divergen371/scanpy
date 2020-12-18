60a0c4a088b4c2fd46ae2a1b3f3856cf1cfd764f
divergen371 <divergen@protonmail.com>
2020-12-19 00:35:41 +0900

    update

--- subdomainScaner.py
+++ subdomainScaner.py
@@ -2,15 +2,34 @@ import requests
 import sys
 
 domain = sys.argv[1]
+file = sys.argv[2]
+output_file_name = sys.argv[3]
 
 
-file = open(sys.argv[2])
+class FileIO:
+    def __init__(self, read, write):
+        self.read = read
+        self.write = write
+        self._discovered_subdomains = []
 
-name_list = file.read()
-subdomains = name_list.splitlines()
+    def list_opener(self):
+        with open(self.read) as file:
+            name_list = file.read()
+            subdomains = name_list.splitlines()
+            return subdomains
 
-discovered_subdomains = []
-for subdomain in subdomains:
+    def file_writing(self):
+        with open(self.write, "w") as f:
+            for subdomain in self._discovered_subdomains:
+                print(subdomain, file=f)
+
+    def discovered_subdomain_nameappend(self, discosub):
+        self._discovered_subdomains.append(discosub)
+
+
+fileIO = FileIO(file, output_file_name)
+
+for subdomain in fileIO.list_opener():
     url = f"http://{subdomain}.{domain}"
     try:
         requests.get(url)
@@ -18,8 +37,4 @@ for subdomain in subdomains:
         pass
     else:
         print("[*] Discovered subdomain: {}".format(url))
-        discovered_subdomains.append(url)
-
-with open("discovered_subdomains_list.txt", "w") as f:
-    for subdomain in discovered_subdomains:
-        print(subdomain, file=f)
+        fileIO.discovered_subdomain_nameappend(url)
